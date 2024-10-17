98d191b8027f54df486c36a7deb6e5c7de17bfbc
mateusscs <mateussepulveda10@gmail.com>
2024-10-17 09:31:43 -0300

    Initial commit

---
+++ src/main/java/dev/mateu/EncurtaLinks/EncurtaLinksApplication.java
@@ -0,0 +1,13 @@
+package dev.mateu.EncurtaLinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mateu/EncurtaLinks/EncurtaLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mateu.EncurtaLinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
